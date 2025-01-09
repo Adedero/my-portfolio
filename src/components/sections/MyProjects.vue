f1d8bc9116f913766a5a17b6cd00b2578c138a55
Adedero <118962395+Adedero@users.noreply.github.com>
2025-01-09 10:42:04 +0100

    Updated project

--- src/components/sections/MyProjects.vue
+++ src/components/sections/MyProjects.vue
@@ -1,4 +1,4 @@
-<script setup>
+np<script setup>
 import { onMounted } from 'vue';
 import ofriend from '@/assets/images/ofriend.png';
 import ofriendWaitlist from '@/assets/images/ofriend-waitlist.png';
@@ -7,21 +7,33 @@ import jesusLabs from '@/assets/images/jesus-labs.png';
 import solarCare from '@/assets/images/solar-care.png';
 import soltracker from '@/assets/images/soltracker.png';
 import cscPortal from '@/assets/images/csc-portal.png';
+import twentiesConnect from '@/assets/images/twenties-connect.png';
 
 const projects = [
   {
     id: 1,
+    title: 'Twenties Connect',
+    year: 2025,
+    tools: ['HTML', 'CSS', 'Tailwind CSS', 'Javascript'],
+    imageUrl: twentiesConnect,
+    demoUrl: 'https://twenties-connect.vercel.app',
+    githubUrl: 'https://github.com/Adedero/twenties-connect',
+    isPrivate: false,
+    description: 'The website was built to publicize the upcoming Twenties Connect Youth Event which is scheduled to hold sometime in 2025. Built with an array of simple tools, the website features a colorful, but not overwhelming design that is meant to attract and grab one\'s attention.'
+  },
+  {
+    id: 2,
     title: 'Ofriend Edu',
     year: 2024,
     tools: ['Tailwind CSS', 'Javascript', 'Vue.Js', 'React.Js', 'Nuxt.Js', 'Node.Js', 'Express.Js', 'MongoDB', 'PostgreSQL', 'Sequelize'],
     imageUrl: ofriend,
-    demoUrl: 'https://ofriend.io',
+    demoUrl: 'https://ofriend.vercel.app/onboarding/signup',
     githubUrl: 'https://github.com/Adedero/ofriend-client',
     isPrivate: false,
     description: 'I developed the Ofriend Educational Module, a comprehensive learning platform that integrates a social media and isPrivate chat system for enhanced student interaction and collaboration. The platform also includes an extensive eLibrary, providing easy access to academic resources. Designed for scalability, security, and user engagement, this project highlights my ability to lead and execute complex, multi-feature platforms that serve educational institutions with seamless performance and user-friendly interfaces.'
   },
   {
-    id: 2,
+    id: 3,
     title: 'Ofriend Waitlist',
     year: 2024,
     tools: ['HTML', 'CSS', 'Tailwind CSS', 'Javascript', 'Node.Js', 'Express.Js', 'EJS', 'SQLite', 'Sequelize'],
@@ -32,7 +44,7 @@ const projects = [
     description: 'This waitlist boasts a simple yet stylish design. I designed and built it to be SEO friendly, so most of its content is server-side generated (SSG), making use of EJS: a templating engine for Node.Js. Also, the webiste uses a simple SQLite database which handles the backend data and the persistence of cookie sessions to track vistors.'
   },
   {
-    id: 3,
+    id: 4,
     title: 'Student Result Portal',
     year: 2023,
     tools: ['Tailwind CSS', 'Javascript', 'Vue.Js', 'Node.Js', 'Express.Js', 'MongoDB'],
@@ -43,7 +55,7 @@ const projects = [
     description: 'This result portal was created for the Department of Computer Science, Federal University of Technology, Owerri as one of the projects I undertook during my final year as an undergaduate. It is a proprietary web app with accounts for students, lecturers and an admin panel. Results can be entered or uploaded by lecturers and viewd by the students. The app also provides a way to manage student activities within the department .'
   },
   {
-    id: 4,
+    id: 5,
     title: 'My Portfolio',
     year: 2024,
     tools: ['Tailwind CSS', 'Vue.Js', 'Javascript'],
@@ -54,7 +66,7 @@ const projects = [
     description: 'Built using Tailwind CSS and Vue, my personal portfolio showcases my development skills and projects in a modern, responsive design. It highlights my expertise in creating intuitive, fast-loading, and aesthetically pleasing websites while maintaining clean and modular code. The portfolio serves as both a personal brand and a testament to my full-stack development capabilities.'
   },
   {
-    id: 5,
+    id: 6,
     title: 'Jesus Labs',
     year: 2024,
     tools: ['HTML', 'CSS', 'Javascript'],
@@ -65,7 +77,7 @@ const projects = [
     description: 'I contributed to the development of the JesusLabs NFT platform, which enables users to mint, buy, and sell NFTs. The platform was designed to offer a seamless experience with blockchain integration, ensuring secure transactions and digital ownership. My work focused on optimizing the user interface to ensure scalability and high performance, bringing digital assets to a broader audience.'
   },
   {
-    id: 6,
+    id: 7,
     title: 'SolarCare',
     year: 2024,
     tools: ['Tailwind CSS', 'Vue.Js', 'Express.Js', 'Node.Js', 'MongoDB'],
@@ -76,7 +88,7 @@ const projects = [
     description: 'I created SolarCare as an undergraduate project. It is a self-diagnosing system for solar panels which helps users find and fix problems with their solar panels.'
   },
   {
-    id: 7,
+    id: 8,
     title: 'SolTracker',
     year: 2024,
     tools: ['Tailwind CSS', 'SCSS', 'Vue.Js', 'Express.Js', 'Node.Js', 'MongoDB'],
